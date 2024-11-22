edefe113e93590e1f3c99bae7c6832bb595589a5
ZaidSaleemHashlogics <zaid.saleem@hashlogics.com>
2024-11-22 06:23:09 +0500

    isAnagram optimal solutions added.

--- src/blind75/Array.java
+++ src/blind75/Array.java
@@ -27,4 +27,41 @@ public class Array {
 
         return Arrays.equals(sArr, tArr);
     }
+
+    public boolean isAnagramByHashMap(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> countS = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> countT = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < s.length(); i++){
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        for (char item : s.toCharArray()) {
+            if (!countS.get(item).equals(countT.get(item))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isAnagramOptimal(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int item : count)
+            if(item != 0) return false;
+
+        return true;
+    }
+
+
 }
